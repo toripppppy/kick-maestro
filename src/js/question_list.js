057d361cb731270067ef91d42a26ea654a5c4a3c
toripppppy <Lundy11@icloud.com>
2024-01-28 16:59:56 +0900

    本体を追加

---
+++ src/js/question_list.js
@@ -0,0 +1,124 @@
+/* 出題リスト
+
+指定の形式で音源ファイルと問題を登録すると出題リストに追加されます。
+問題を追加する際は以下の形式に従ってください。
+
+音源ファイルを assets/audio に格納してください。ただし対応しているファイル形式は wav, mp3 のみです。
+音源ファイルの作成は、 https://ytmp3.nu/CNtD/ などの YouTube -> mp3 変換サイトが便利です。
+
+音源を格納したら、以下の形式に従ってQUESTION_LISTに要素を追加してください。
+
+{
+  "name": String
+  - "コンポーザー名 - 曲名" の形式で入力してください。
+  - 答えとして表示されるものです。
+
+  "drop": Array[Number]
+  - 出題する始点となる秒数を指定できます。キックが鳴り始める瞬間を指定してください。
+  - 必ず秒数で計算して記載してください。（1m15sなら75, 3m38sなら218のように）
+  - 値を複数指定すると、出題時にランダムで一つ選択され出題されます。
+  - 曲中の音色の異なるキックを全て登録するとバリエーションが生まれて難易度が上がり面白いです。^^
+  - キック以外を含むパートを登録してしまうと難易度が下がります。(-o-)
+
+  "url": String
+  - 音源ファイルへのパスまたはリンクを記載してください。
+  - 相対パス・絶対パス・URLに対応していますが、相対パスを推奨します。
+  - 対応しているファイル形式は wav, mp3 のみです。
+}
+
+他の問題と同様に登録できていれば大丈夫です。
+リロードしてゲームをお楽しみください。
+*/ 
+
+export const QUESTION_LIST = [
+  {
+    "name": "TOZA & Stratisphere - WARZONE",
+    "drop": [34, 52, 139, 153],
+    "url": "./assets/audio/WARZONE.mp3"
+  },
+  {
+    "name": "Malice - MF KING",
+    "drop": [30, 42, 48],
+    "url": "./assets/audio/Malice - MF KING (Official Video).mp3"
+  },
+  {
+    "name": "Rebelion - In Control",
+    "drop": [70, 82, 184],
+    "url": "./assets/audio/Rebelion - In Control (Official Video).mp3"
+  },
+  {
+    "name": "Riot Shift - PIECES",
+    "drop": [81, 93],
+    "url": "./assets/audio/Riot Shift - PIECES (Official Video).mp3"
+  },
+  {
+    "name": "Malice - MY THERAPY",
+    "drop": [40, 52, 64, 136],
+    "url": "./assets/audio/Malice - MY THERAPY (Official Video).mp3"
+  },
+  {
+    "name": "BATTLE NO.1 (DJ Myosuke Remix)",
+    "drop": [67, 117],
+    "url": "./assets/audio/TANOC Sound Team - BATTLE NO.1 (DJ Myosuke Remix).mp3"
+  },
+  {
+    "name": "DJ Myosuke - Megaton Keeper",
+    "drop": [39, 160],
+    "url": "./assets/audio/DJ Myosuke - Megaton Keeper.mp3"
+  },
+  {
+    "name": "Malice - Stronger",
+    "drop": [63, 75, 153],
+    "url": "./assets/audio/Malice - Stronger (Official Videoclip).mp3"
+  },
+  {
+    "name": "RoughSketch - Ill",
+    "drop": [44, 57],
+    "url": "./assets/audio/RoughSketch - Ill.mp3"
+  },
+  {
+    "name": "Riot Shift - SAME AGAIN",
+    "drop": [87, 99],
+    "url": "./assets/audio/Riot Shift - SAME AGAIN (Official Music Video).mp3"
+  },
+  {
+    "name": "Yuta Imai - UNKNOWN LEVELS(Extended Mix)",
+    "drop": [27, 45, 80, 194],
+    "url": "./assets/audio/Yuta Imai - UNKNOWN LEVELS (Extended Mix).mp3"
+  },
+  {
+    "name": "Hard Driver - Blood Sugar",
+    "drop": [66, 78, 84, 135, 141, 147, 153],
+    "url": "./assets/audio/Hard Driver - Blood Sugar (Official Hardstyle Video).mp3"
+  },
+  {
+    "name": "Sub Zero Project & Villain - The Solution",
+    "drop": [69],
+    "url": "./assets/audio/Sub Zero Project & Villain - The Solution (Official Video).mp3"
+  },
+  {
+    "name": "Neroz & Irradiate - Forged In Fury",
+    "drop": [31, 41, 63, 152, 165, 223],
+    "url": "./assets/audio/Neroz & Irradiate - Forged In Fury (Official Videoclip).mp3"
+  },
+  {
+    "name": "Bloodlust & Regain - GET UP",
+    "drop": [35, 51, 128, 147, 159],
+    "url": "./assets/audio/Bloodlust & Regain - GET UP (Official Video).mp3"
+  },
+  {
+    "name": "Warface - Taste The Blade",
+    "drop": [60, 78, 168],
+    "url": "./assets/audio/Warface - Taste The Blade (Official Video).mp3"
+  },
+  {
+    "name": "Warface & Code Black - Here Forever",
+    "drop": [70, 85, 91, 160, 172],
+    "url": "./assets/audio/Warface & Code Black - Here Forever (Official Video).mp3"
+  },
+  {
+    "name": "BCM - #BassHouse",
+    "drop": [75, 105, 175],
+    "url": "./assets/audio/BCM - BassHouse.mp3"
+  },
+]
---
+++ src/js/script.js
@@ -0,0 +1,254 @@
+import WaveSurfer from "https://unpkg.com/wavesurfer.js/dist/wavesurfer.esm.js"
+import RegionsPlugin from "https://unpkg.com/wavesurfer.js/dist/plugins/regions.esm.js"
+
+import { QUESTION_LIST } from "./question_list.js";
+
+const display = document.getElementById("display")
+const button = document.getElementById("button")
+const onemore = document.getElementById("onemore")
+const waveform = document.getElementById("waveform")
+
+let state = "first";
+let question = null;
+let questionMemory = [];
+let canClick = true;
+
+const wavesurfer = WaveSurfer.create({
+  container: '#waveform',
+  waveColor: 'rgb(200, 0, 200)',
+  progressColor: 'rgb(100, 0, 100)',
+  // 表示幅
+  minPxPerSec: 250,
+  // スクロールバーを非表示
+  hideScrollbar: true,
+  // 触れても何も起きない
+  interact: false,
+  // ノーマライズ
+  normalize: true,
+})
+
+const wsRegions = wavesurfer.registerPlugin(RegionsPlugin.create())
+
+// 音源の読み込み完了後に呼び出される
+wavesurfer.on('decode', () => {
+  // リージョン（マーカー）を全てクリアする
+  wsRegions.clearRegions()
+})
+
+// 表示非表示を設定する
+// target: element
+const setHidden = (target, flag) => {
+  if (flag) {
+    target.style.visibility = "hidden";
+  } else {
+    target.style.visibility = "visible";
+  }
+}
+
+// ボタンを押せるかどうか
+const setCanClick = (flag) => {
+  canClick = flag;
+}
+
+/// WaveSurfer
+const setTime = (time) => {
+  wavesurfer.setTime(time)
+}
+
+const play = (time = null) => {
+  if (time !== null) {
+    setTime(time);
+  } else {
+    setTime(question["drop"])
+  }
+  wavesurfer.play()
+}
+
+const pause = () => {
+  wavesurfer.pause()
+}
+
+/// テキスト更新
+const setDisplayText = (text) => {
+  display.innerText = text;
+}
+
+const setButtonText = (text) => {
+  button.innerText = text;
+}
+
+/// ゲームの進行
+const introduction = () => {
+  setDisplayText("始めようか");
+  setButtonText("待ち遠しいです");
+}
+
+const waitForQuestion = () => {
+  // カウントダウン中はボタンは押せない
+  setCanClick(false);
+  setButtonText("来るぞ・・・");
+  countdown(3, guessStart);
+}
+
+const guessStart = () => {
+  setCanClick(true);
+  // 波形を表示
+  setHidden(waveform, false);
+  play(question["drop"]);
+  setDisplayText("答えよ");
+  setButtonText("理解りました");
+}
+
+const answer = () => {
+  pause();
+  setDisplayText("名は何という");
+  setButtonText("答えを見る");
+  // もう一回聞くボタンを表示
+  setHidden(onemore, false);
+}
+
+const showAnswer = () => {
+  // 出題開始地点にマーカーを設置
+  wsRegions.clearRegions()
+  setMarker("Question", question["drop"]);
+  // ドロップの５秒前から流して答え合わせ
+  play(question["drop"] - 5);
+  setDisplayText(`${question["name"]}`);
+  setButtonText("当然です");
+}
+
+const goToNextQuestion = () => {
+  // もう一回聞くボタンを非表示
+  setHidden(onemore, true);
+  // 再生停止して初期化
+  pause();
+  init();
+  setDisplayText("準備はいいか");
+  setButtonText("待ち遠しいです");
+}
+
+const oneMore = () => {
+  if (state === "answered") {
+    setHidden(onemore, true);
+    state = "guessing";
+    waitForQuestion();
+  }
+  else if (state === "checkingAnswer") {
+    pause();
+    setTimeout(showAnswer, 1000);
+  }
+}
+
+const onclick = () => {
+  if (!canClick) return;
+  
+  if (state === "first") {
+    state = "waitForQuestion";
+    introduction();
+  }
+  else if (state === "waitForQuestion") {
+    state = "guessing";
+    waitForQuestion();
+  }
+  else if (state === "guessing") {
+    state = "answered"
+    answer();
+  }
+  else if (state === "answered") {
+    state = "checkingAnswer"
+    showAnswer();
+  }
+  else if (state === "checkingAnswer") {
+    state = "waitForQuestion"
+    goToNextQuestion();
+  }
+}
+
+button.onclick = onclick;
+onemore.onclick = oneMore;
+
+document.addEventListener('keydown', (e) => {
+  // スペースキーもボタンとして使用可能
+  if (e.code === "Space") {
+    onclick();
+  }
+});
+
+init();
+
+function countdown(count, callback) {
+  let i = count;
+  setDisplayText(i)
+  i--;
+
+  const interval = setInterval(() => {
+    setDisplayText(i)
+    i--;
+
+    if (i < 0) {
+      clearInterval(interval);
+      callback()
+    }
+  }, 1000);
+}
+
+function makeQuestion() {
+  // 出題リストからランダムな問題を取得
+  const index = Math.floor(Math.random() * QUESTION_LIST.length);
+  const new_question = QUESTION_LIST[index]
+
+  // 直近5題の重複を防ぐ
+  const maxMemory = 5;
+
+  if (questionMemory.includes([new_question["name"],new_question["drop"]])) {
+    // 再帰
+    return makeQuestion();
+  }
+  // 直近5題を記憶
+  questionMemory.push([new_question["name"],new_question["drop"]])
+  if (questionMemory.length > maxMemory) {
+    questionMemory.shift()
+  }
+
+  // debug
+  console.log(new_question)
+  
+  // ランダムなドロップを選択
+  const drop_index = Math.floor(Math.random() * new_question["drop"].length);
+
+  // 問題にフォーマットして返す
+  return {
+    "name": new_question["name"],
+    "drop": new_question["drop"][drop_index],
+    "url": new_question["url"],
+  }
+}
+
+// 登録されたdropの合計を返す
+function sumOfPatterns() {
+  return QUESTION_LIST.reduce((sum, q) => sum + q["drop"].length, 0);
+}
+
+function init() {
+  // もろもろの非表示
+  setHidden(waveform, true);
+  setHidden(onemore, true);
+  // 問題を作成し設定
+  setQuestion(makeQuestion());
+  // タイトルコール
+  setDisplayText(`キックマエストロ（現在${QUESTION_LIST.length}曲・${sumOfPatterns()}パターン収録）`);
+}
+
+function setQuestion(new_question) {
+  // 問題を作成し、wavesurferに登録する
+  question = new_question;
+  wavesurfer.load(new_question["url"]);
+}
+
+function setMarker(text, time) {
+  wsRegions.addRegion({
+    start: time,
+    content: text,
+    color: "#cccccc",
+  })
+}
