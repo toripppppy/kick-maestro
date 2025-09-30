ec2b137d4fe711b0fa2d1958b997a78ef356aeed
toripppppy <lundy11@icloud.com>
2025-09-30 11:19:49 +0900

    ジャンル対応に向けてフォルダ構造を整理

--- src/js/question_list.js
+++ src/js/question_list.js
@@ -34,136 +34,136 @@ export const QUESTION_LIST = [
   {
     "name": "TOZA & Stratisphere - WARZONE",
     "drop": [34, 52, 139, 153],
-    "url": "src/assets/audio/WARZONE.mp3"
+    "url": "src/assets/audio/hardstyle/WARZONE.mp3"
   },
   {
     "name": "Malice - MF KING",
     "drop": [30, 42, 48],
-    "url": "src/assets/audio/Malice - MF KING (Official Video).mp3"
+    "url": "src/assets/audio/hardstyle/Malice - MF KING (Official Video).mp3"
   },
   {
     "name": "Rebelion - In Control",
     "drop": [70, 82, 184],
-    "url": "src/assets/audio/Rebelion - In Control (Official Video).mp3"
+    "url": "src/assets/audio/hardstyle/Rebelion - In Control (Official Video).mp3"
   },
   {
     "name": "Riot Shift - PIECES",
     "drop": [81, 93],
-    "url": "src/assets/audio/Riot Shift - PIECES (Official Video).mp3"
+    "url": "src/assets/audio/hardstyle/Riot Shift - PIECES (Official Video).mp3"
   },
   {
     "name": "Malice - MY THERAPY",
     "drop": [40, 52, 64, 136],
-    "url": "src/assets/audio/Malice - MY THERAPY (Official Video).mp3"
+    "url": "src/assets/audio/hardstyle/Malice - MY THERAPY (Official Video).mp3"
   },
   {
     "name": "BATTLE NO.1 (DJ Myosuke Remix)",
     "drop": [67, 117],
-    "url": "src/assets/audio/TANOC Sound Team - BATTLE NO.1 (DJ Myosuke Remix).mp3"
+    "url": "src/assets/audio/hardstyle/TANOC Sound Team - BATTLE NO.1 (DJ Myosuke Remix).mp3"
   },
   {
     "name": "DJ Myosuke - Megaton Keeper",
     "drop": [39, 160],
-    "url": "src/assets/audio/DJ Myosuke - Megaton Keeper.mp3"
+    "url": "src/assets/audio/hardstyle/DJ Myosuke - Megaton Keeper.mp3"
   },
   {
     "name": "Malice - Stronger",
     "drop": [63, 75, 153],
-    "url": "src/assets/audio/Malice - Stronger (Official Videoclip).mp3"
+    "url": "src/assets/audio/hardstyle/Malice - Stronger (Official Videoclip).mp3"
   },
   {
     "name": "RoughSketch - Ill",
     "drop": [44, 57],
-    "url": "src/assets/audio/RoughSketch - Ill.mp3"
+    "url": "src/assets/audio/hardstyle/RoughSketch - Ill.mp3"
   },
   {
     "name": "Riot Shift - SAME AGAIN",
     "drop": [87, 99],
-    "url": "src/assets/audio/Riot Shift - SAME AGAIN (Official Music Video).mp3"
+    "url": "src/assets/audio/hardstyle/Riot Shift - SAME AGAIN (Official Music Video).mp3"
   },
   {
     "name": "Yuta Imai - UNKNOWN LEVELS(Extended Mix)",
     "drop": [27, 45, 80, 194],
-    "url": "src/assets/audio/Yuta Imai - UNKNOWN LEVELS (Extended Mix).mp3"
+    "url": "src/assets/audio/hardstyle/Yuta Imai - UNKNOWN LEVELS (Extended Mix).mp3"
   },
   {
     "name": "Hard Driver - Blood Sugar",
     "drop": [66, 78, 84, 135, 141, 147, 153],
-    "url": "src/assets/audio/Hard Driver - Blood Sugar (Official Hardstyle Video).mp3"
+    "url": "src/assets/audio/hardstyle/Hard Driver - Blood Sugar (Official Hardstyle Video).mp3"
   },
   {
     "name": "Sub Zero Project & Villain - The Solution",
     "drop": [69],
-    "url": "src/assets/audio/Sub Zero Project & Villain - The Solution (Official Video).mp3"
+    "url": "src/assets/audio/hardstyle/Sub Zero Project & Villain - The Solution (Official Video).mp3"
   },
   {
     "name": "Neroz & Irradiate - Forged In Fury",
     "drop": [31, 41, 63, 152, 165, 223],
-    "url": "src/assets/audio/Neroz & Irradiate - Forged In Fury (Official Videoclip).mp3"
+    "url": "src/assets/audio/hardstyle/Neroz & Irradiate - Forged In Fury (Official Videoclip).mp3"
   },
   {
     "name": "Bloodlust & Regain - GET UP",
     "drop": [35, 51, 128, 147, 159],
-    "url": "src/assets/audio/Bloodlust & Regain - GET UP (Official Video).mp3"
+    "url": "src/assets/audio/hardstyle/Bloodlust & Regain - GET UP (Official Video).mp3"
   },
   {
     "name": "Warface - Taste The Blade",
     "drop": [60, 78, 168],
-    "url": "src/assets/audio/Warface - Taste The Blade (Official Video).mp3"
+    "url": "src/assets/audio/hardstyle/Warface - Taste The Blade (Official Video).mp3"
   },
   {
     "name": "Warface & Code Black - Here Forever",
     "drop": [70, 85, 91, 160, 172],
-    "url": "src/assets/audio/Warface & Code Black - Here Forever (Official Video).mp3"
+    "url": "src/assets/audio/hardstyle/Warface & Code Black - Here Forever (Official Video).mp3"
   },
   {
     "name": "BCM - #BassHouse",
     "drop": [75, 105, 175],
-    "url": "src/assets/audio/BCM - BassHouse.mp3"
+    "url": "src/assets/audio/hardstyle/BCM - BassHouse.mp3"
   },
   {
     "name": "Headhunterz & Vertile ft. Sian Evans - Lost Without You",
     "drop": [117, 229],
-    "url": "src/assets/audio/Headhunterz & Vertile ft. Sian Evans - Lost Without You (Defqon.1 2023 Closing Theme).mp3"
+    "url": "src/assets/audio/hardstyle/Headhunterz & Vertile ft. Sian Evans - Lost Without You (Defqon.1 2023 Closing Theme).mp3"
   },
   {
     "name": "Sefa - 1527",
     "drop": [136, 146],
-    "url": "src/assets/audio/Sefa - 1527 (Official Video).mp3"
+    "url": "src/assets/audio/hardstyle/Sefa - 1527 (Official Video).mp3"
   },
   {
     "name": "Headhunterz & Vertile - Before I Wake",
     "drop": [44, 57, 69],
-    "url": "src/assets/audio/Headhunterz & Vertile - Before I Wake (Official Videoclip).mp3"
+    "url": "src/assets/audio/hardstyle/Headhunterz & Vertile - Before I Wake (Official Videoclip).mp3"
   },
   {
     "name": "DJ Mad Dog - Reset",
     "drop": [19, 28, 33, 52, 62, 136, 146, 156, 165],
-    "url": "src/assets/audio/DJ Mad Dog - Reset.mp3"
+    "url": "src/assets/audio/hardstyle/DJ Mad Dog - Reset.mp3"
   },
   {
     "name": "The Purge & Adjuzt ft. RXBY  - Summer Secrets",
     "drop": [71, 89, 155, 167],
-    "url": "src/assets/audio/The Purge & Adjuzt ft. RXBY  - Summer Secrets  Q-dance Records.mp3"
+    "url": "src/assets/audio/hardstyle/The Purge & Adjuzt ft. RXBY  - Summer Secrets  Q-dance Records.mp3"
   },
   {
     "name": "MINUS MILITIA - GAS D'R OP",
     "drop": [21, 61, 102],
-    "url": "src/assets/audio/MINUS MILITIA - GAS D'R OP (Official Video).mp3"
+    "url": "src/assets/audio/hardstyle/MINUS MILITIA - GAS D'R OP (Official Video).mp3"
   },
   {
     "name": "The Prophet - Wanna Play?",
     "drop": [47, 59, 195, 205],
-    "url": "src/assets/audio/The Prophet - Wanna Play？ (Official Videoclip).mp3"
+    "url": "src/assets/audio/hardstyle/The Prophet - Wanna Play？ (Official Videoclip).mp3"
   },
   {
     "name": "Atmozfears - The Soul",
     "drop": [144],
-    "url": "src/assets/audio/Atmozfears - The Soul.mp3"
+    "url": "src/assets/audio/hardstyle/Atmozfears - The Soul.mp3"
   },
   {
     "name": "Killshot - Monster",
     "drop": [58, 73, 155, 167, 182],
-    "url": "src/assets/audio/Killshot - Monster.mp3"
+    "url": "src/assets/audio/hardstyle/Killshot - Monster.mp3"
   },
 ]
